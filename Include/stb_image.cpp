5a196faf7f9ab17884707a1162382880c6c49dfb
Steffen Kromm <kromm.steffen@gmail.com>
2018-05-20 11:00:07 +0200

    Removed not used or not implemented files and folders

--- Include/stb_image.cpp
+++
@@ -1,3 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-//#define STBI_ASSERT(x)
-#include "third_party/stb_image/stb_image.h"
--- Source/xml/xml.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "private/tinyxml2.h"
-
-namespace xml
-{
-	using Document = tinyxml2::XMLDocument;
-	using Element = tinyxml2::XMLElement;
-}
